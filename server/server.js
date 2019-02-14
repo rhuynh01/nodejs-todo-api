597474a4b9d9825ed4c886f440e59396f9dfd185
Rodney Huynh <rhuynh2005@gmail.com>
2019-02-14 14:05:06 -0600

    Test POST /todos route'

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user')
 var app = express()
 
 app.use(bodyParser.json())
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -20,9 +21,8 @@ app.post('/todos', (req, res) => {
   })
 })
 
-
-
-
 app.listen(3000, () => {
   console.log('Started on port 3000')
 })
+
+module.exports = {app}
