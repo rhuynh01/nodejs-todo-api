6f2bcdf4446beb01a5828dbe228cb9960a5bff57
Rodney Huynh <rhuynh2005@gmail.com>
2019-02-14 07:39:51 -0600

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server')
+  }
+  console.log('Connected to MongoDB server')
+
+  // findOneAndUpdate (Todos)
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID("5c6569f3949b3fbba5c39075")
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result)
+  // })
+
+  // findOneAndUpdate (Users)
+  db.collection('Users').findOneAndUpdate(
+    { location: 'Tulsa'},
+    { 
+      $set: { name: 'Rodney_updated'},
+      $inc: { age: 1}
+    },
+    {returnOriginal: false}
+  ).then((result) => {
+    console.log(result)
+  })
+
+  // db.close()
+})
