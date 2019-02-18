c8011f5a23b86f188c747661e3f55dc8181bd149
Rodney Huynh <rhuynh2005@gmail.com>
2019-02-18 08:09:39 -0600

    Add POST /users route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 
+
 const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -90,6 +91,20 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo: todo})
   }).catch((e) => {
     res.status(400).send()
+  })
+})
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
   })
 })
 
