b6479b5c42701fda2a75fbc9bc676e602e35d662
Rodney Huynh <rhuynh2005@gmail.com>
2019-02-18 12:19:22 -0600

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -62,9 +62,6 @@ UserSchema.statics.findByToken = function (token) {
   try {
     decoded = jwt.verify(token, 'abc123')
   } catch (e) {
-    // return new Promise((resolve, reject) => {
-    //   reject()
-    // })
     return Promise.reject()
   }
 
@@ -74,6 +71,29 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   })
 }
+
+UserSchema.statics.findByCredential = function(email, password) {
+  var User = this
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject()
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user)
+        } else {
+          reject()
+        }
+      })
+    })
+  })
+}
+// bcrypt.compare(password, hashedPassword, (err, res) => {
+//   console.log(res)
+// })
+
 
 UserSchema.pre('save', function (next) {
   var user = this
